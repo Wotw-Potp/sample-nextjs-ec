f9f0be2765a35192d5d017abc0d63a4a9e4262e5
Wotw-Potp <s.otsuka08@gmail.com>
2023-08-27 17:42:20 +0900

    :arrow_up: update stripe api-version

--- project/src/providers/stripeClientProvider.ts
+++ project/src/providers/stripeClientProvider.ts
@@ -1,10 +1,10 @@
-import Stripe from "stripe"
+import Stripe from 'stripe'
 
 const stripeApiPublicKey = process.env.STRIPE_API_KEY_SECRET ?? ''
 
 export default function StripeClientProvider() {
   const stripe = new Stripe(stripeApiPublicKey, {
-    apiVersion: '2022-11-15',
+    apiVersion: '2023-08-16',
     maxNetworkRetries: 3,
   })
 
